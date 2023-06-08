dc26bfff0057f1af3edbf1e22a67f1769d95ec5f
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-06-09 01:12:18 +0530

    Set Matrix Zeros brute force

--- Learn the basics/passByValueReference.java
+++
@@ -1,7 +0,0 @@
-// package "Learn the basics";
-class Solution {
-    static int[] passedBy(int a, int b) {
-        // return array without assigning variable to it
-        return new int[]{(a+1),(b+2)};
-    }
-}
